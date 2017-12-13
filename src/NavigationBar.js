0b343d7d0882fad1e313cb2f9f1b0817c13e5c92
Janusz Pietraga <januszpietraga@gmail.com>
2017-12-13 16:38:10 +0100

    Navigation-bar

---
+++ src/NavigationBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+inpor {
+
+}
+
+
+
+class NavigationBar extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Heder>
+          <Navbar.Brand>
+
+          </Navbar.Brand>
+        </Navbar.Heder>
+      </Navbar>
+    )
+  }
+}
