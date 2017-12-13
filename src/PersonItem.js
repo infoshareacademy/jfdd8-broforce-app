8bbdba5079e870a33a9e8c160a566b7d28812235
mrdampc <marcindampc@gmail.com>
2017-12-13 16:35:23 +0100

    routing init

---
+++ src/PersonItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class PersItem extends React.Component {
+  render() {
+    return (<div>Person Items</div>
+
+    )
+  }
+}
+export default PersItem
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import PersonItem from './PersonItem';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/personscreen" component={PersonItem}/>
+
+        <PersonItem/>
+
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
 registerServiceWorker();
