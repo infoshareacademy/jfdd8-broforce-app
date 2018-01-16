709bb68400bc9ddb93eb08f5252cdabe36c7e7a8
michal <info@iammo.pl>
2018-01-16 17:01:10 +0100

    menu czeki

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -8,78 +8,76 @@ import foodItems from './foodItems'
 
 class FoodItem extends React.Component {
 
-    state = {
-        selectedFoodItemIds: []
-    }
+  state = {
+    selectedFoodItemIds: []
+  }
 
-    handleChange = event => {
+  handleChange = event => {
 
-        const foodItemId = event.target.dataset.foodItemId
+    const foodItemId = event.target.dataset.foodItemId
 
-        const foodIsChecked = this.state.selectedFoodItemIds.find(
-            item => item === foodItemId
-        ) !== undefined;
+    const foodIsChecked = this.state.selectedFoodItemIds.find(
+      item => item === foodItemId
+    ) !== undefined;
 
-        const checkedFoodId = this.state.selectedFoodItemIds.filter(
-            item => item !== foodItemId
-        );
+    const checkedFoodId = this.state.selectedFoodItemIds.filter(
+      item => item !== foodItemId
+    );
 
+    this.setState({
+      selectedFoodItemIds: foodIsChecked ? checkedFoodId : this.state.selectedFoodItemIds.concat(foodItemId)
+    })
+  };
+
+  handleOrder = event => {
+    const userUid = firebase.auth().currentUser.uid
+    firebase.database().ref('/orders/' + userUid).set({
+      selectedFoodItemIds: this.state.selectedFoodItemIds.toString(),
+      time: Date.now().toString()
+    })
+  }
+
+  componentDidMount() {
+    const userUid = firebase.auth().currentUser.uid;
+
+
+    firebase.database().ref('/orders/' + userUid).on(
+      'value',
+      snapshot => {
+        const snapshotValue = snapshot.val();
         this.setState({
-            selectedFoodItemIds: foodIsChecked ? checkedFoodId : this.state.selectedFoodItemIds.concat(foodItemId)
-        })
-    };
-
-    handleOrder = event => {
-        const userUid = firebase.auth().currentUser.uid
-        firebase.database().ref('/orders/' + userUid).set({
-            selectedFoodItemIds: this.state.selectedFoodItemIds.toString(),
-            time: Date.now().toString()
-        })
-    }
-
-    componentDidMount() {
-        const userUid = firebase.auth().currentUser.uid;
+          selectedFoodItemIds: snapshotValue === null ? [] : snapshotValue.selectedFoodItemIds ? snapshotValue.selectedFoodItemIds.split(",") : []
+        });
+      }
+    )
+  }
 
 
-        firebase.database().ref('/orders/' + userUid).on(
-            'value',
-            snapshot => {
-                const snapshotValue = snapshot.val();
-                this.setState({
-                    selectedFoodItemIds: snapshotValue === null ? [] : snapshotValue.selectedFoodItemIds ? snapshotValue.selectedFoodItemIds.split(",") : []
-                });
-            }
-        )
-    }
+  render() {
 
+    return (
+      <div className="menu">
+        {
+          foodItems.map(
+            foodItem => (
+              <label className="container-czek">
+                <input
+                  type="checkbox"
+                  onChange={this.handleChange}
+                  checked={this.state.selectedFoodItemIds.includes(foodItem.id)}
+                  data-food-item-id={foodItem.id}
+                />
+                  <span class="checkmark"></span>
+                {foodItem.name} - {foodItem.price} PLN
 
-    render() {
-
-        return (
-            <div className="menu">
-                {
-                    foodItems.map(
-                        foodItem => (
-                            <label className="container-czek">
-
-
-
-                              <input
-                                  type="checkbox"
-                                  onChange={this.handleChange}
-                                  checked={this.state.selectedFoodItemIds.includes(foodItem.id)}
-                                  data-food-item-id={foodItem.id}
-                              />
-                              <span class="checkmark"></span>
-                                {foodItem.name} - {foodItem.price} PLN
-                            </label>
-                        )
-                    )
-                }
-              <button className="login-button" onClick={this.handleOrder}>Zamów</button>
-            </div>
-        )
-    }
+              </label>
+            )
+          )
+        }
+        <button className="login-button" onClick={this.handleOrder}>Zamów</button>
+      </div>
+    )
+  }
 }
 
 
