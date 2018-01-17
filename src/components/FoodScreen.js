097894863c93b6665f1c4f63455d246d89c3c188
michal <info@iammo.pl>
2018-01-17 16:04:27 +0100

    Merge branch 'develop' of /home/michal/WebstormProjects/jfdd8-broforce-app with conflicts.

--- src/components/FoodScreen.js
+++ src/components/FoodScreen.js
@@ -4,18 +4,18 @@ import LogOut from "./LogOut";
 import OrderButtonNavigation from './OrderButtonNavigation';
 
 class FoodScreen extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LogOut/>
-        <header className="App-header">
-          <h1 className="App-title">Wybierz szamę</h1>
-        </header>
-          <FoodItem/>
-        <OrderButtonNavigation/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <LogOut/>
+                <header className="App-header">
+                    <h1 className="App-title">Wybierz szamę</h1>
+                </header>
+                <FoodItem/>
+                <OrderButtonNavigation/>
+            </div>
+        );
+    }
 }
 
 export default FoodScreen;
