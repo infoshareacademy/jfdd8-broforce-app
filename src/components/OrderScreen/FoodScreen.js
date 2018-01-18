9265af62d37d3e2f49f9a0a7491cd051ee163623
mrdampc <marcindampc@gmail.com>
2018-01-18 16:53:10 +0100

    clear all button wip

--- src/components/OrderScreen/FoodScreen.js
+++ src/components/OrderScreen/FoodScreen.js
@@ -2,20 +2,32 @@ import React, {Component} from 'react';
 import FoodItem from './FoodItem';
 import LogOut from "../LogOut";
 import OrderButtonNavigation from '../OrderButtonNavigation';
+import '../App.css';
 
 class FoodScreen extends Component {
-    render() {
-        return (
-            <div className="App">
-                <LogOut/>
-                <header className="App-header">
-                    <h1 className="App-title">Wybierz szamę</h1>
-                </header>
-                <FoodItem/>
-                <OrderButtonNavigation/>
-            </div>
-        );
-    }
+
+  handleClearAll = event => {
+    let emptyArray = [];
+
+    this.props.selectedFoodItemIds = emptyArray;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <LogOut/>
+        <header className="App-header">
+          <h1 className="App-title">Wybierz szamę</h1>
+        </header>
+        <FoodItem/>
+        <button
+          className="login-button"
+          onClick={this.handleClearAll}
+        >clear all</button>
+        <OrderButtonNavigation/>
+      </div>
+    );
+  }
 }
 
 export default FoodScreen;
